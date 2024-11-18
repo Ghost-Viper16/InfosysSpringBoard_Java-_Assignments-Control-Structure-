e10a92cfe3f03142ee7649084766fa64fd4c18db
Kumar Kartikey Choudhary <choudharykartik733@gmail.com>
2024-11-18 23:03:16 +0530

    Update Assignment6.java
    resolved some issue

--- Assignment6.java
+++ Assignment6.java
@@ -3,78 +3,79 @@ import java.util.Scanner;
 class Assignment6 {
 	public static void main(String[] args) {
 		Scanner sc  = new Scanner(System.in);
-		System.out.println("Enter the Date to print the next Date");
-		int date = sc.nextInt();
+		System.out.println("Enter the Day to print the next Day");
+		int day = sc.nextInt();
 		System.out.println("Enter the Month");
 		int month =  sc.nextInt();
 		System.out.println("Enter the Year");
 		int year =  sc.nextInt();
-		if(month==2)
+	        if(month == 2)
 		{
-		    if(date>=1 && date <28)
+		    if(day >= 1 && day < 28)
 		    {
-		        date ++;
-		        System.out.println(date+"-"+month+"-"+year);
+		        day++;
+		        System.out.println(day+"-"+month+"-"+year);
 		    }
-		    else if(date==28 && (year %4) ==0)
+		    else if(day == 28 && (year % 4) == 0)
 		    {
-		        date++;
-		        System.out.println(date+"-"+month+"-"+year);
+		        day++;
+		        System.out.println(day+"-"+month+"-"+year);
 		    }
-		    else  if(date==28 || date ==29)
+		    else  if(day == 28 || day == 29)
 		    {
-		        date=1;
+		        day = 1;
 		        month++;
-		        System.out.println(date+"-"+month+"-"+year);
+		        System.out.println(day+"-"+month+"-"+year);
 		    }
 		     else
 		    {
-		        System.out.println("you are giving wrong date and month");
+		        System.out.println("you are giving wrong day and month");
 		    }
 		}
-		else if(month == 1 || month == 3 || month ==5 || month ==7 || month == 8 || month == 10 || month ==12)
+		else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
 		{
-		    if(date>=1 && date<31)
+		    if(day >= 1 && day < 31)
 		    {
-		        date++;
-		        System.out.println(date+"-"+month+"-"+year);
+		        day++;
+		        System.out.println(day+"-"+month+"-"+year);
 		    }
-		    else if(date==31 && month!=12)
+		    else if(day == 31 && month != 12)
 		    {
-		        date =1 ;
+		        day = 1 ;
 		        month++;
-		        System.out.println(date+"-"+month+"-"+year);
+		        System.out.println(day+"-"+month+"-"+year);
 		    }
-		    else if(month == 12 && date==31)
+		    else if(month == 12 && day == 31)
 		    {
-		        date =1 ;
+		        day = 1 ;
 		        month = 1;
 		        year++;
-		        System.out.println(date+"-"+month+"-"+year);
+		        System.out.println(day+"-"+month+"-"+year);
 		    }
 		    else
 		    {
-		        System.out.println("you are giving wrong date and month");
+		        System.out.println("you are giving wrong day and month");
 		    }
 		}
-		else if(month== 4 || month== 6 || month== 9|| month == 11)
+		else if(month == 4 || month == 6 || month == 9|| month == 11)
 		{
-		     if(date>=1 && date<31)
+		     if(day >= 1 && day < 31)
 		    {
-		        date++;
-		        System.out.println(date+"-"+month+"-"+year);
+		        day++;
+		        System.out.println(day+"-"+month+"-"+year);
 		    }
-		    else if(date==31)
+		    else if(day == 31)
 		    {
-		        date =1 ;
+		        day = 1 ;
 		        month++;
-		        System.out.println(date+"-"+month+"-"+year);
+		        System.out.println(day+"-"+month+"-"+year);
 		    }
 		     else
 		    {
-		        System.out.println("you are giving wrong date and month");
+		        System.out.println("you are giving wrong day and month");
 		    }
 		}
 	}
 }
+
 
